e5b4a9d17c21bd82dece1dc0d845125a966306d0
devsheke <abhi@sheke.tech>
2025-01-07 23:50:04 +0530

    feat: add statuspadding patch

--- config.def.h
+++ config.def.h
@@ -3,16 +3,10 @@
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappih = 20;  /* horiz inner gap between windows */
-static const unsigned int gappiv = 10;  /* vert inner gap between windows */
-static const unsigned int gappoh =
-    10; /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov =
-    30; /* vert outer gap between windows and screen edge */
-static int smartgaps =
-    0; /* 1 means no outer gap when there is only one window */
-static const int showbar = 1; /* 0 means no bar */
-static const int topbar = 1;  /* 0 means bottom bar */
+static const int showbar = 1;           /* 0 means no bar */
+static const int topbar = 1;            /* 0 means bottom bar */
+static const int horizpadbar = 2;       /* horizontal padding for statusbar */
+static const int vertpadbar = 0;        /* vertical padding for statusbar */
 static const char *fonts[] = {"monospace:size=10"};
 static const char dmenufont[] = "monospace:size=10";
 static const char col_gray1[] = "#222222";
@@ -20,6 +14,15 @@ static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#005577";
+static const unsigned int gappih = 20; /* horiz inner gap between windows */
+static const unsigned int gappiv = 10; /* vert inner gap between windows */
+static const unsigned int gappoh =
+    10; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov =
+    30; /* vert outer gap between windows and screen edge */
+static int smartgaps =
+    0; /* 1 means no outer gap when there is only one window */
+
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
